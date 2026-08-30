f0ffd7698f4072c0c74b5a4eaeb7c3530603fd4d
Mohan K <mohan7gen@gmail.com>
2026-08-30 23:49:49 +0530

    Fix: Bump PWA cache version to force UI update

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
  * ============================================================
  */
 
-const CACHE_NAME = 'godzemohan-v2.6';
+const CACHE_NAME = 'godzemohan-v2.7';
 const ASSETS_TO_CACHE = [
 	'/',
 	'/index.html',
